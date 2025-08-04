57a7e8652c697bc091ac227acbac6ddf42a83487
Serega5555 <lazarpalace@gmail.com>
2025-08-04 11:54:50 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace Module_17._6._6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Account regularAccount = new RegularAccount
+            {
+                Balance = 800
+            };
+            regularAccount.CalculateInterest();
+            Console.WriteLine($"Проценты для обычного счета: {regularAccount.Interest}");
+
+            Account salaryAccount = new SalaryAccount
+            {
+                Balance = 1500
+            };
+            salaryAccount.CalculateInterest();
+            Console.WriteLine($"Проценты для зарплатного счета: {salaryAccount.Interest}");
+        }
+    }
+}
