76929922ab2f5fe6cb329528e1faba3cddbfd1ca
Serega5555 <lazarpalace@gmail.com>
2025-08-04 11:53:59 +0300

    Create Account.cs

---
+++ Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_17._6._6
+{
+    public abstract class Account
+    {
+        public double Balance { get; set; }
+        public double Interest { get; protected set; }
+        public abstract void CalculateInterest();
+    }
+}
