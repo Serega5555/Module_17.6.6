96a8fa9e2f35f6fea68c6341a973c25df73b405f
Serega5555 <lazarpalace@gmail.com>
2025-08-04 11:55:26 +0300

    Create RegularAccount.cs

---
+++ RegularAccount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_17._6._6
+{
+    public class RegularAccount: Account
+    {
+        public override void CalculateInterest()
+        {
+            Interest = Balance * 0.4;
+
+            if (Balance < 1000)
+                Interest -= Balance * 0.2;
+            else if (Balance >= 1000)
+                Interest -= Balance * 0.4;
+        }
+    }
+}
