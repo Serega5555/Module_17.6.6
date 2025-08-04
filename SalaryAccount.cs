7a2b6bfec88cfc3f20928e395db866b156c13892
Serega5555 <lazarpalace@gmail.com>
2025-08-04 11:55:53 +0300

    Create SalaryAccount.cs

---
+++ SalaryAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_17._6._6
+{
+    public class SalaryAccount: Account
+    {
+        public override void CalculateInterest()
+        {
+            Interest = Balance * 0.5;
+        }
+    }
+}
